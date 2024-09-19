f25184b553527e4f62ac9dc9920d15ab56b02dea
aeps71 <66871203+aeps71@users.noreply.github.com>
2024-09-19 19:18:26 +0700

    Update Bab_1_Geografi.py

--- pages/Bab_1_Geografi.py
+++ pages/Bab_1_Geografi.py
@@ -11,7 +11,7 @@ st.header(":blue[BAB 1 GEOGRAFI]")
 st.subheader("", divider='rainbow')
 
 datakk = pd.read_excel("data/geografi_desa.xlsx")
-sort_datakk = datakk.sort_values(by=['tahun', 'namakab', 'namakec', 'namadesa'], ascending=[False,False,True,True])
+sort_datakk = datakk.sort_values(by=['tahun', 'namakab', 'namakec', 'namadesa', 'luas_desa'], ascending=[False,False,True,True])
         
 pilihankab = sort_datakk['namakab'].unique()
 
@@ -49,7 +49,7 @@ with kol1d:
 
 # JUMLAH KK
 with st.container(border=True):
-    st.info(f"{tabelkk2.iloc[0,2]}, Luas Wilayah Desa di Kecamatan {pilihkec}, {pilihkab} Tahun {pilihtahun} (Km2)")
+    st.info(f"{sort_datakk.iloc[0,2]}, Luas Wilayah Desa di Kecamatan {pilihkec}, {pilihkab} Tahun {pilihtahun} (Km2)")
     kol1d, kol1e, kol1f = st.columns(3)
     if pilihkab and pilihkec and pilihtahun:
         tabelkk = datakk[(datakk['namakab'] == pilihkab) & (datakk['namakec'] == pilihkec) & (datakk['tahun'] == pilihtahun)]

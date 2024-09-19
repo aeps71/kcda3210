663b43507c1da6bdde1af651a13e95a97030d89c
aeps71 <66871203+aeps71@users.noreply.github.com>
2024-09-19 18:34:14 +0700

    Update Bab_1_Geografi.py

--- pages/Bab_1_Geografi.py
+++ pages/Bab_1_Geografi.py
@@ -50,7 +50,7 @@ with kol1d:
 # JUMLAH KK
 with st.container(border=True):
     for index, row in sort_datakk.iterrows():
-    st.info(f"Desa {row['namadesa']}, Luas Wilayah Desa di Kecamatan {pilihkec}, {pilihkab} Tahun {pilihtahun} (Km2)")
+    st.info(f"Desa {namadesa}, Luas Wilayah Desa di Kecamatan {pilihkec}, {pilihkab} Tahun {pilihtahun} (Km2)")
     kol1d, kol1e, kol1f = st.columns(3)
     if pilihkab and pilihkec and pilihtahun:
         tabelkk = datakk[(datakk['namakab'] == pilihkab) & (datakk['namakec'] == pilihkec) & (datakk['tahun'] == pilihtahun)]

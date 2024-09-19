be19b2904a7a47e94f5f3cce62783104719072be
aeps71 <66871203+aeps71@users.noreply.github.com>
2024-09-19 20:16:15 +0700

    Update Bab_1_Geografi.py

--- pages/Bab_1_Geografi.py
+++ pages/Bab_1_Geografi.py
@@ -83,7 +83,7 @@ with st.container(border=True):
             bar_kk2.update_layout(showlegend=False)
             with st.container(border=True):
                 st.plotly_chart(bar_kk2, use_container_width=True)
-            with st.container(border=True):
+        with st.container(border=True):
             desapertama = tabelkk2.iloc[0,0]
             st.subheader(f":green[DESA {desapertama}] ADALAH DESA TERLUAS DI KECAMATAN {pilihkec}, {pilihkab}")    
 
